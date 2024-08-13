baaead5eb92168720d3b292487349d5d449f6701
akhil-06 <svam1008@gmail.com>
2024-08-13 21:00:23 +0530

    login and signup done

--- day23_ls&ss/script.js
+++ day23_ls&ss/script.js
@@ -1,98 +1,73 @@
 function validateForm() {
-  let name = document.forms["RegForm"]["Name"];
-  let email = document.forms["RegForm"]["EMail"];
-  let password = document.forms["RegForm"]["Password"];
-  let confirm_password = document.forms["RegForm"]["Confirm"];
+  var name = document.forms["RegForm"]["Name"];
+  var email = document.forms["RegForm"]["EMail"];
+  var password = document.forms["RegForm"]["Password"];
+  var confirmPassword = document.forms["RegForm"]["Confirm"];
 
-  // console.log(name.value);
-
-  if (name.value === "") {
-    alert("Please enter your name");
+  if (name.value == "") {
+    window.alert("Please enter your name.");
+    name.focus();
     return false;
   }
 
-  if (email.value === "") {
-    alert("Please enter your email");
+  if (email.value == "") {
+    window.alert("Please enter a valid e-mail address.");
+    email.focus();
     return false;
   }
 
-  if (password.value === "") {
-    alert("Please enter your password");
+  if (password.value == "") {
+    window.alert("Please enter your password");
+    password.focus();
     return false;
   }
 
-  if (confirm_password.value === "") {
-    alert("Please enter your confirm Password");
+  if (confirmPassword.value == "") {
+    window.alert("Please enter your password");
+    password.focus();
     return false;
   }
 
-  if (password.value === confirm_password.value) {
-    if (stroedInLocalStorage()) {
-      alert("Registered Succesfully");
+  if (password.value === confirmPassword.value) {
+    if (register()) {
+      window.alert("Registration is done successfully!!!");
       return true;
     } else {
       return false;
     }
   } else {
-    alert("Password and Confirm Password do not match");
+    window.alert("Password and confirm password should be same");
     return false;
   }
+
   return true;
 }
 
-let arr = [];
+var users = JSON.parse(localStorage.getItem("usersList"))
+  ? JSON.parse(localStorage.getItem("usersList"))
+  : [];
+console.log(users);
+function register() {
+  var name = document.forms["RegForm"]["Name"].value;
+  var email = document.forms["RegForm"]["EMail"].value;
+  var password = document.forms["RegForm"]["Password"].value;
+  var confirmPassword = document.forms["RegForm"]["Confirm"].value;
 
-function stroedInLocalStorage(){
-    console.log("Stored in local storage!!!");
-    let name = document.forms["RegForm"]["Name"].value;
-    let email = document.forms["RegForm"]["EMail"].value;
-    let password = document.forms["RegForm"]["Password"].value;
-    let confirm_password = document.forms["RegForm"]["Confirm"].value;
+  var newUser = {
+    id: Number(new Date()),
+    name: name,
+    email: email,
+    password: password,
+    confirmPassword: confirmPassword,
+  };
 
-    let userObj = {
-        name: name,
-        email: email,
-        password: password,
-        confirm_password: confirm_password
-    }
+  var userExists = users.some((user) => user.email === newUser.email);
+  if (userExists) {
+    return new Error({ error: "User exists" });
+  } else {
+    users.push(newUser);
+  }
 
-    arr.push(userObj);
-
-    let userArrString = JSON.stringify(arr);
-    localStorage.setItem("users", userArrString);
-    // return true;
+  var usersString = JSON.stringify(users);
+  localStorage.setItem("usersList", usersString);
 }
-
-
-
-
-// sessionStorage.setItem("name", "Akhil");
-// sessionStorage.setItem("age", "25");
-
-// let data = sessionStorage.getItem("age");
-// let data1 = sessionStorage.getItem("name");
-
-// console.log(data, data1);
-
-// // let a = 10;
-// let obj = {
-//     name:"Akhil",
-//     age:25,
-//     city:"Hyderabad"
-// }
-
-// let arr = [1,2,3,4,56,7,78, obj];
-// arr[4]=60000
-
-// localStorage.setItem("object", JSON.stringify(obj));
-// let data = JSON.parse(localStorage.getItem("object"));
-
-// localStorage.setItem("array", JSON.stringify(arr));
-
-// let data1 = JSON.parse(localStorage.getItem("array"));
-
-// // localStorage.removeItem("object")
-// // localStorage.clear()
-
-// console.log(data);
-// console.log(data1);
